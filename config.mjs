e1ea37ab5fc6e19e9fcf94cb85147c4e8e45d0a8
Sam <sambowenhughes@hotmail.co.uk>
2025-03-26 09:29:28 +0800

    Increase disk space by default for AWS Lambda

--- config.mjs
+++ config.mjs
@@ -6,5 +6,5 @@ export const REGION = "us-east-1";
 
 export const SITE_NAME = "sams-site";
 export const RAM = 3009;
-export const DISK = 2048;
+export const DISK = 10240;
 export const TIMEOUT = 240;
